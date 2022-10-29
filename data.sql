ab591e8272c22584fcaca2449501c4092c232a22
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-10-29 16:06:24 +0600

    add join table scripts and query screenshots

--- data.sql
+++ data.sql
@@ -1,58 +1,162 @@
-/* Populate database with sample data. */
+/* Insert the following data:
+  Animal: His name is Agumon. He was born on Feb 3rd, 2020, and currently weighs 10.23kg. He was neutered and he has never tried to escape.
+  Animal: Her name is Gabumon. She was born on Nov 15th, 2018, and currently weighs 8kg. She is neutered and she has tried to escape 2 times.
+  Animal: His name is Pikachu. He was born on Jan 7th, 2021, and currently weighs 15.04kg. He was not neutered and he has tried to escape once.
+  Animal: Her name is Devimon. She was born on May 12th, 2017, and currently weighs 11kg. She is neutered and she has tried to escape 5 times.
+*/
 
-INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg )
-VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
-       ('Gabumon', '2018-11-15', 2, true, 8),
-       ('Pikachu', '2021-01-07', 1, false, 15.04),
-       ('Devimon', '2017-05-12', 5, true, 11),
-       ('Charmander', '2020-02-8', 0, false, -11),
-       ('Plantmon', '2021-11-15', 2, true, -5.7),
-       ('Squirtle', '1993-04-02', 3, false, -12.13),
-       (' Angemon', '2005-06-12', 1, true, -45),
-       ('Boarmon', '2005-06-07', 7, true, 20.4),
-       ('Blossom.', '1998-10-13', 3, true, 17),
-       ('Ditto', '2022-04-14', 4, true, 22);
+-- We can add the single data like below again and again
 
--- Inserting data into the owners table:
-INSERT INTO owners (full_name, age)
-VALUES ('Sam Smith', 34),
-       ('Jennifer Orwell', 19),
-       (' Bob', 45),
-       ('Melody Pond', 77),
-       ('Dean Winchester', 14),
-       ('Jodie Whittaker', 38);
-
--- Inserting data into the species table: 
-INSERT INTO species (name)
-VALUES ('Pokemon'),('Digimon');
-
--- Modify your inserted animals so it includes the species_id value: 
--- If the name ends in "mon" it will be Digimon
-UPDATE animals SET species_id = (
-    SELECT id FROM species WHERE name= 'Digimon')
-    WHERE name LIKE '%mon';
-    SELECT * FROM animals;
+insert into animals 
+  (name, date_of_birth, escape_attempts, neutered, weight_kg) values
+  ('Agumon', '03-FEB-2020', 0, TRUE, 10.23)
+;
 
 
-UPDATE animals SET species_id = (
-SELECT id FROM species WHERE name ='Pokemon') 
-WHERE name NOT LIKE '%mon';
-SELECT * FROM animals;
+-- We can add the multiple data or entry by using list of tuples method
 
--- Modify your inserted animals to include owner information (owner_id): 
+insert into animals 
+  (name, date_of_birth, escape_attempts, neutered, weight_kg) values
+  ('Gabumon', '15-NOV-2018', 2, TRUE, 8),
+  ('Pikachu', '07-JAN-2021', 1, FALSE, 15.04),
+  ('Devimon', '12-MAY-2017', 5, TRUE, 11)
+;
 
-    -- Sam Smith owns Agumon.
-    UPDATE animals SET owner_id =( SELECT id FROM owners WHERE full_name ='Sam Smith') WHERE name = ' Agumon';
+/*
+  Insert the following data:
+  Animal: His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
+  Animal: Her name is Plantmon. She was born on Nov 15th, 2021, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
+  Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to escape 3 times.
+  Animal: His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
+  Animal: His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
+  Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
+  Animal: His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.
+*/ 
 
-    -- Jennifer Orwell owns Gabumon and Pikachu.
-    UPDATE animals SET owner_id =( SELECT id FROM owners WHERE full_name ='Jennifer Orwell') WHERE name = 'Gabumon' OR name = 'Pikachu';
+insert into animals
+  (name, date_of_birth, escape_attempts, neutered, weight_kg) values
+  ('Charmander', '08-FEB-2020', 0, false, -11),
+  ('Plantmon', '15-NOV-2021', 2, true, -5.7),
+  ('Squirtle', '02-APR-1993', 3, false, -12.13),
+  ('Angemon', '12-JUN-2005', 1, true, -45),
+  ('Boarmon', '07-JUN-2005', 7, true, 20.4),
+  ('Blossom', '13-OCT-1998', 3, true, 17),
+  ('Ditto', '14-MAY-2022', 4, true, 22)
+;
 
-    -- Bob owns Devimon and Plantmon.
-     UPDATE animals SET owner_id =( SELECT id FROM owners WHERE full_name =' Bob') WHERE name = 'Devimon' OR name = 'Plantmon';
-    -- Melody Pond owns Charmander, Squirtle, and Blossom.
-    UPDATE animals SET owner_id =( SELECT id FROM owners WHERE full_name ='Melody Pond') WHERE name = 'Blossom' OR name = 'Squirtle' OR name = 'Charmander';
-    -- Dean Winchester owns Angemon and Boarmon.
-         UPDATE animals SET owner_id =( SELECT id FROM owners WHERE full_name ='Dean Winchester') WHERE name = ' Angemon' AND name = 'Boarmon';
+/* 
+  Insert the following data into the owners table:
+    Sam Smith 34 years old.
+    Jennifer Orwell 19 years old.
+    Bob 45 years old.
+    Melody Pond 77 years old.
+    Dean Winchester 14 years old.
+    Jodie Whittaker 38 years old.
+*/
 
--- UPDATE animals SET owner_id = 5 WHERE name = ' Angemon';
+insert into owners 
+  (full_name, age) values
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38)
+;
+
+/*
+  Insert the following data into the species table:
+    Pokemon
+    Digimon
+*/
+
+insert into species (name) values ('Pokemon'), ('Digimon');
+
+/*
+  Modify your inserted animals so it includes the species_id value:
+    If the name ends in "mon" it will be Digimon
+    All other animals are Pokemon
+*/
+
+update animals set species_id = 2 where name like '%mon';
+update animals set species_id = 1 where name not like '%mon';
+
+/*
+  Modify your inserted animals to include owner information (owner_id):
+    Sam Smith owns Agumon.
+    Jennifer Orwell owns Gabumon and Pikachu.
+    Bob owns Devimon and Plantmon.
+    Melody Pond owns Charmander, Squirtle, and Blossom.
+    Dean Winchester owns Angemon and Boarmon.
+*/
+update animals set owner_id = (select id from owners where full_name like 'Sam Smith') where name like 'Agu%';
+update animals set owner_id = (select id from owners where full_name like '%well') where name like 'Gabu%' or name like 'Pika%';
+update animals set owner_id = (select id from owners where full_name like 'Bob') where name like 'Devi%' or name like 'Plan%';
+update animals set owner_id = (select id from owners where full_name like 'Mel%') where name in ('Charmander', 'Squirtle', 'Blossom');
+update animals set owner_id = (select id from owners where full_name like 'Dean%') where name in ('Angemon', 'Boarmon');
+
+/*
+  Insert the following data for vets:
+    Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+    Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+    Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+    Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+*/
+
+insert into vets 
+  (name, age, date_of_graduation) values
+  ('William Tatcher', 45, '23-APR-2000'),
+  ('Maisy Smith', 26, '17-JAN-2019'),
+  ('Stephanie Mendez', 64, '04-MAY-1981'),
+  ('Jack Harkness', 38, '08-JUN-2008')
+;
+
+/*
+  Insert the following data for specialties:
+    Vet William Tatcher is specialized in Pokemon.
+    Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+    Vet Jack Harkness is specialized in Digimon.
+*/
+
+insert into specializations
+  (vet_id, species_id) values
+  (1, 1),
+  (3, 2),
+  (3, 1),
+  (4, 2)
+;
+
+/* 
+  Insert the following data for visits:
+    Agumon: 1, Gabumon: 2, Pikachu: 3, 
+    Devimon: 4, Charmander: 5, Plantmon: 6, 
+    Squirtle: 7, Angemon: 8, Boarmon: 9, Blossom: 10
+
+    William Tatcher: 1, Maisy Smith: 2, 
+    Stephanie Mendez: 3, Jack Harkness: 4
+*/
+
+insert into visits
+  (animal_id, vet_id, last_visit) values
+  (1, 1, '24-MAY-2020'),
+  (1, 3, '22-JUL-2020'),
+  (2, 4, '02-FEB-2021'),
+  (3, 2, '05-JAN-2020'),
+  (3, 2, '08-MAR-2020'),
+  (3, 2, '14-MAY-2020'),
+  (4, 3, '04-MAY-2021'),
+  (5, 4, '24-FEB-2021'),
+  (6, 2, '21-DEC-2019'),
+  (6, 1, '10-AUG-2020'),
+  (6, 2, '07-APR-2021'),
+  (7, 3, '29-SEP-2019'),
+  (8, 4, '03-OCT-2020'),
+  (8, 4, '04-NOV-2020'),
+  (9, 2, '24-JAN-2019'),
+  (9, 2, '15-MAY-2019'),
+  (9, 2, '27-FEB-2020'),
+  (9, 2, '03-AUG-2020'),
+  (10, 3, '24-MAY-2020'),
+  (10, 1, '11-JAN-2021')
+;
 
--- queries.sql
+++ queries.sql
@@ -1,65 +1,168 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*
+  Find all animals whose name ends in "mon".
+  List the name of all animals born between 2016 and 2019.
+  List the name of all animals that are neutered and have less than 3 escape attempts.
+  List the date of birth of all animals named either "Agumon" or "Pikachu".
+  List name and escape attempts of animals that weigh more than 10.5kg
+  Find all animals that are neutered.
+  Find all animals not named Gabumon.
+  Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+*/
 
-SELECT *  FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
-SELECT date_of_birth from animals WHERE name ='Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered=true;
-SELECT * FROM animals WHERE NOT name='Gabumon';
-SELECT * FROM animals WHERE weight_kg >= '10.4' and weight_kg <='17.3';
+-- Find all animals whose name ends in "mon".
+select * from animals where name like '%mon';
 
--- update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
-BEGIN;
-UPDATE animals SET species = 'unspecified';
-ROLLBACK;
-SELECT * FROM animals;
+-- List the name of all animals born between 2016 and 2019.
+select name from animals where
+date_of_birth >= '2016/01/01' 
+and 
+date_of_birth <= '2019/01/01';
 
--- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name  LIKE '%mon';
-UPDATE animals SET species ='pokemon' WHERE  species IS null;
-COMMIT;
-SELECT * FROM animals;
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+select name from animals where neutered is true and escape_attempts < 3;
 
--- delete all records in the animals table, then roll back the transaction.
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-SELECT * FROM animals;
+-- Alternate way for above query
+select name from animals where neutered=true and escape_attempts < 3;
 
--- Delete all animals born after Jan 1st, 2022.
+-- List the date of birth of all animals named either "Agumon" or "Pikachu".
+select date_of_birth from animals where name like 'Agumon' or name like 'Pikachu';
 
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT FIRST_POINT;
-UPDATE animals SET weight_kg = weight_kg * (-1);
-ROLLBACK TO FIRST_POINT;
-UPDATE animals SET weight_kg = weight_kg * (-1) WHERE weight_kg < 0;
-COMMIT;
-SELECT * FROM animals;
+-- List name and escape attempts of animals that weigh more than 10.5kg
+select name from animals where weight_kg > 10.5;
 
--- Write queries to answer the following questions:
-SELECT COUNT(*) as total_animals FROM animals;
-SELECT COUNT(*) as zero_escape_attempts FROM animals WHERE escape_attempts = '0';
-SELECT AVG(weight_kg) as average_animal_weight FROM animals;
-SELECT neutered, MAX(escape_attempts) FROM ANIMALS GROUP BY neutered;
-SELECT MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
-SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth  BETWEEN '1990-01-01'AND '2000-01-01'  GROUP BY species;;
+-- Find all animals that are neutered.
+select * from animals where neutered is true;
 
--- Write queries (using JOIN) to answer the following questions: 
+-- Find all animals not named Gabumon.
+select * from animals where name != 'Gabumon';
 
-    -- What animals belong to Melody Pond?
-    SELECT * FROM animals JOIN owners ON animals.owner_id = owners.id AND owners.full_name = 'Melody Pond';
-    -- List of all animals that are pokemon (their type is Pokemon).
-    SELECT * FROM animals JOIN species ON animals.species_id = species.id AND species.name = 'pokemon';
-    -- List all owners and their animals, remember to include those that don't own any animal.
-    SELECT * FROM animals RIGHT JOIN owners ON animals.owner_id = owners.id; 
-    -- How many animals are there per species?
-    SELECT COUNT(*), species.name FROM animals JOIN species on animals.species_id = species.id GROUP BY species.id; 
-    -- List all Digimon owned by Jennifer Orwell.
-    SELECT * FROM animals JOIN owners ON animals.owner_id = owners.id JOIN species ON animals.species_id = species.id AND owners.full_name = 'Jennifer Orwell' AND species.name ='Digimon';
-    -- List all animals owned by Dean Winchester that haven't tried to escape.
-    SELECT * FROM animals JOIN owners ON animals.owner_id = owners.id and owners.full_name ='Dean Winchester' and animals.escape_attempts = 0;
-    -- Who owns the most animals?
-SELECT owners.full_name, COUNT(animals) FROM owners JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(animals) DESC LIMIT 1;
+-- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+select * from animals where weight_kg between 10.4 and 17.3;
+
+/*
+  Inside a transaction update the animals table by setting the species column to unspecified. 
+  Verify that change was made. Then roll back the change and verify that the species columns 
+  went back to the state before the transaction.
+*/
+
+begin;
+update animals set species = 'unspecified';
+select * from animals;
+rollback;
+
+/*
+  Inside a transaction:
+  Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+  Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+  Commit the transaction.
+  Verify that change was made and persists after commit.
+*/
+
+begin;
+update animals set species = 'digimon' where name like '%mon';
+select * from animals;
+update animals set species = 'pokemon' where name not like '%mon';
+select * from animals;
+commit;
+select * from animals;
+
+/*
+  Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+  After the rollback verify if all records in the animals table still exists. After that, you can start breathing as usual ;)
+*/
+
+begin;
+delete from animals;
+select * from animals;
+rollback;
+select * from animals;
+
+/*
+  Inside a transaction:
+  Delete all animals born after Jan 1st, 2022.
+  Create a savepoint for the transaction.
+  Update all animals' weight to be their weight multiplied by -1.
+  Rollback to the savepoint
+  Update all animals' weights that are negative to be their weight multiplied by -1.
+  Commit transaction
+*/
+
+begin;
+delete from animals where date_of_birth > '01-JAN-2022';
+savepoint label_1;
+update animals set weight_kg = weight_kg * -1;
+select * from animals;
+rollback to label_1;
+select * from animals;
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+select * from animals;
+commit;
+select * from animals;
+
+-- How many animals are there?
+select count(*) from animals;
+
+-- How many animals have never tried to escape?
+select count(*) from animals where escape_attempts = 0;
+
+-- What is the average weight of animals?
+select avg(weight_kg) as average_weight from animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+select neutered, sum(escape_attempts) from animals group by neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+select min(weight_kg) as "minimum_animals_weight (kg)", max(weight_kg) as "maximum_animals_weight (kg)", species from animals group by species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+select avg(escape_attempts) as "Average of Escaped Animal Per Species", species as "Type" from animals where date_of_birth between '01-JAN-1990' and '01-01-2000' group by species;
+
+-- What animals belong to Melody Pond?
+select animal.name as "Animal Name", owner.full_name as "Owner Name" from animals animal join owners owner on owner.id = animal.owner_id where owner.full_name like 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+select animal.name as "Animal Name", type.name as "Species Type" from animals animal join species type on animal.species_id = type.id where type.name like 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+select owner.full_name as "Owner", animal.name as "Owned" from animals animal right join owners owner on animal.owner_id = owner.id;
+
+-- How many animals are there per species?
+select type.name as "Type", count(animal.*) as "Total Animals" from animals animal right join species type on animal.species_id = type.id group by type.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+select animal.name as "Animal Name", type.name as "Type of Animal" from animals animal join species type on animal.species_id = type.id join owners owner on animal.owner_id = owner.id where owner.full_name like 'Jennifer Orwell' and type.name like 'Digimon';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+select animal.name as "Animal", owner.full_name from animals animal left join owners owner on animal.owner_id = owner.id where owner.full_name like 'Dean Winchester' and animal.escape_attempts=0;
+
+-- Who owns the most animals?
+select owner.full_name as "Owner Name", count(animal.*) as "Total Owned (Animals)" from animals animal left join owners owner on animal.owner_id = owner.id group by owner.full_name order by "Total Owned (Animals)" desc limit 1;
+
+-- DAY 4: Join-Tables Queries:
+
+-- Who was the last animal seen by William Tatcher?
+select vet.name as "Vet Name", animal.name as "Animal Name", visit.last_visit as "Last Visit (date)" from animals animal join visits visit on animal.id = visit.animal_id join vets vet on vet.id = visit.vet_id where vet.name like 'William%' order by visit.last_visit desc limit 1;
+
+-- How many different animals did Stephanie Mendez see?
+select count(visit.animal_id) as "Total Animal Saw (By Stephanie Mendez)" from animals animal join visits visit on animal.id = visit.animal_id join vets vet on vet.id = visit.vet_id where vet.name like 'Stephanie%' group by vet.name;
+
+-- List all vets and their specialties, including vets with no specialties.
+select vet.name as "Vet Name", type.name as "Specialize In" from vets vet left join specializations skill on skill.vet_id = vet.id left join species type on type.id = skill.species_id order by vet.name asc;
+
+-- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+select animal.name as "Animal Name", vet.name as "Vet Name", visit.last_visit as "Last Visited" from animals animal join visits visit on visit.animal_id = animal.id join vets vet on vet.id = visit.vet_id where vet.name like 'Stephanie%' and visit.last_visit between '01-APR-2020' and '30-AUG-2020' order by animal.name asc;
+
+-- What animal has the most visits to vets?
+select animal.name as "Animal Name", count(visit.animal_id) as "Visited Time", vet.name as "Vet's Name" from animals animal join visits visit on visit.animal_id = animal.id join vets vet on vet.id = visit.vet_id group by ("Animal Name", "Vet's Name") order by "Visited Time" desc limit 1;
+
+-- Who was Maisy Smith's first visit?
+select animal.name as "Animal Name", visit.last_visit as "First Visited (to Maisy Smith)" from animals animal join visits visit on visit.animal_id = animal.id join vets vet on vet.id = visit.vet_id where vet.name like 'Maisy%' order by visit.last_visit asc limit 1;
+
+-- Details for most recent visit: animal information, vet information, and date of visit.
+select animal.name as "Animal's Name", type.name as "Type of Animal", to_char(animal.date_of_birth, 'Mon-DD,YYYY') as "Date Of Birth", animal.weight_kg as "Weight (Kg)", vet.name as "Specialist Vet's Name", vet.age as "Age of Vet", to_char(vet.date_of_graduation, 'Mon-DD,YYYY') as "Graduation Date", to_char(visit.last_visit, 'Mon-DD,YYYY') as "Animal's Recent Visit" from animals animal join species type on animal.species_id = type.id join visits visit on visit.animal_id = animal.id join vets vet on vet.id = visit.vet_id order by "Animal's Recent Visit" desc limit 1;
+
+-- How many visits were with a vet that did not specialize in that animal's species?
+select count(visit.vet_id) as "Not Specialization" from species type join specializations expertise on expertise.species_id <> type.id join visits visit on visit.vet_id = expertise.vet_id;
+
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+select type.name as "Species Name", count(visit.animal_id) as "Visited Time", vet.name as "Vet's Name" from animals animal join visits visit on visit.animal_id = animal.id join vets vet on vet.id = visit.vet_id join species type on type.id = animal.species_id group by ("Species Name", "Vet's Name") order by "Visited Time" desc limit 1;
